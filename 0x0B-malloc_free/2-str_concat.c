dd09a536847023ee24c0a31f916d9cc14889681b
bamansour1978 <karatman@gmail.com>
2023-04-11 23:38:52 -0700

    2. He who is not courageous enough to take risks will accomplish nothing in life

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -1,56 +1,38 @@
 #include "main.h"
 #include <stdlib.h>
-
 /**
+ * *str_concat - a function that concatenates two strings.
  *
+ *@s1: string
  *
+ *@s2: string
  *
- *
- *
- *
- *
- */
-
-int _strlen(char *s)
-{
-	int size = 0;
-	while (s[size] != '\0')
-		size++;
-	return (size + 1);
-}
-
-/**
- *
- *
- *
- *
- *
- *
- *
+ *Return: charactere
  */
 
 char *str_concat(char *s1, char *s2)
 {
-	int siz1, siz2, i, j;
+	int index, ar_index = 0, len = 0;
 	char *ar;
 
 	if (s1 == NULL)
-		s1 = "\0";
+		s1 = "";
 	if (s2 == NULL)
-		s2 = "\0";
+		s2 = "";
 
-	siz1 = _strlen(s1);
-	siz2 = _strlen(s2);
+	for (index = 0; s1[index] || s2[index]; index++)
+		len++;
 
-	ar = (char *) malloc(((siz1 + siz2) - 1) * sizeof(char));
+	ar = malloc(len * sizeof(char));
 	if (ar == NULL)
 		return (NULL);
 
-	for (i = 0; s1[i] != '\0'; i++)
-		ar[i] = s1[i];
-	for (j = 0; s2[j] != '\0';j++)
-                ar[i] = s2[j];
-	ar[i] = '\0';
-	return (ar);
-}	
 
+	for (index = 0; s1[index]; index++)
+			ar[ar_index++] = s1[index];
+
+	for (index = 0; s2[index]; index++)
+		ar[ar_index++] = s2[index];
+
+	return (ar);
+}
