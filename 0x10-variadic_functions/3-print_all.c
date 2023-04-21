ba5f5341d463f7a9f2a8c97a701cfa7eaf4de0f0
bamansour1978 <karatman@gmail.com>
2023-04-20 18:37:34 -0700

    3. To be is a to be the value of a variable

---
+++ 0x10-variadic_functions/3-print_all.c
@@ -0,0 +1,111 @@
+#include "variadic_functions.h"
+#include <stdarg.h>
+#include <stdio.h>
+
+void print_char(va_list arg);
+void print_int(va_list arg);
+void print_float(va_list arg);
+void print_string(va_list arg);
+void print_all(const char * const format, ...);
+
+/**
+ * print_char - ..
+ * @arg: ..
+ */
+
+void print_char(va_list arg)
+{
+	char letter;
+
+	letter = va_arg(arg, int);
+	printf("%c", letter);
+}
+
+/**
+ * print_int - ..
+ * @arg: ..
+ */
+
+void print_int(va_list arg)
+{
+	int number;
+
+	number = va_arg(arg, int);
+	printf("%d", number);
+}
+
+/**
+ * print_float - ..
+ * @arg: ..
+ */
+
+void print_float(va_list arg)
+{
+	float number;
+
+	number = va_arg(arg, double);
+	printf("%f", number);
+}
+
+/**
+ * print_string - ..
+ * @arg: ..
+ */
+
+void print_string(va_list arg)
+{
+	char *str;
+
+	str = va_arg(arg, char *);
+
+	if (str == NULL)
+	{
+		printf("(nil)");
+		return;
+	}
+	else
+		printf("%s", str);
+}
+
+/**
+ * print_all - ..
+ * @format: ..
+ * @...: --
+ */
+
+void print_all(const char * const format, ...)
+{
+	va_list args;
+	int i = 0, j = 0;
+	char *separator = "";
+	printer_t cifs[] = {
+		{"c", print_char},
+		{"i", print_int},
+		{"f", print_float},
+		{"s", print_string}
+	};
+
+	va_start(args, format);
+
+	while (format && (*(format + i)))
+	{
+		j = 0;
+
+		while (j < 4 && (*(format + i) != *(cifs[j].symbol)))
+			j++;
+
+		if (j < 4)
+		{
+			printf("%s", separator);
+			cifs[j].print(args);
+			separator = ", ";
+		}
+
+		i++;
+	}
+
+
+	printf("\n");
+
+	va_end(args);
+}
