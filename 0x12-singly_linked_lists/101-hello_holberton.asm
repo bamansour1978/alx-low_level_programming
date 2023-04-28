5dde4d0eb5ebbeb64a3b91653d506b6bf17814f1
bamansour1978 <karatman@gmail.com>
2023-04-27 19:00:47 -0700

    6. Real programmers can write assembly code in any language

--- 0x12-singly_linked_lists/101-hello_holberton.asm
+++ 0x12-singly_linked_lists/101-hello_holberton.asm
@@ -1,9 +1,15 @@
-global  main
-	  extern  printf
+global main
+extern printf
+
+section .data
+    msg db "Hello, Holberton", 10, 0
+    fmt db "%s", 0
+
+section .text
 main:
-mov   edi, format
-xor   eax, eax
-call  printf
-mov   eax, 0
-ret
-format: db "Hello, Helberton\n",0
+    mov rdi, fmt    ; Premier argument pour printf est l'adresse de la chaîne de format
+    mov rsi, msg    ; Deuxième argument pour printf est l'adresse de la chaîne de caractères
+    xor eax, eax    ; Le registre eax doit être mis à zéro pour que printf fonctionne correctement
+    call printf     ; Appeler la fonction printf
+    mov rax, 0      ; Mettre la valeur de retour à zéro
+    ret             ; Terminer le programme
