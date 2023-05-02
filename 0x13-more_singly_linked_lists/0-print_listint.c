2ae80aca0e44a30c178232c4be1f7178977e1a40
bamansour1978 <karatman@gmail.com>
2023-05-01 23:03:21 -0700

    updated

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -12,10 +12,11 @@ size_t print_listint(const listint_t *h)
 {
 	size_t Num_nodes = 0;
 
-	do {
+	while (h)
+	{
 		printf("%d\n", h->n);
 		Num_nodes++;
 		h = h->next;
-	} while (h);
+	}
 	return (Num_nodes);
 }
