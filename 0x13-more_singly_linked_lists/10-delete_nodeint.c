5db25fc72a4e8ba5d65e98919a4710f43ca719f4
bamansour1978 <karatman@gmail.com>
2023-05-03 15:29:10 -0700

    Updated

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -28,9 +28,9 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 
 	for (i = 0; i < index - 1; i++)
 	{
-		if (!tmp || !(tmp->next))
-			return (1);
-		tmp = tmp->next;
+	if (!tmp || !(tmp->next))
+	return (1);
+	tmp = tmp->next;
 	}
 
 	h_current = tmp->next;
