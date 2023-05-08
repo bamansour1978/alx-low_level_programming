cea609fea4ad2422338818457125481925f804cd
bamansour1978 <karatman@gmail.com>
2023-05-08 15:48:44 -0700

    Creat areadme and header files

---
+++ 0x15-file_io/main.h
@@ -0,0 +1,17 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+#include <stdio.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <fcntl.h>
+#include <string.h>
+
+
+int _putchar(char c);
+ssize_t read_textfile(const char *filename, size_t letters);
+
+
+
+#endif/*MAIN_H*/
