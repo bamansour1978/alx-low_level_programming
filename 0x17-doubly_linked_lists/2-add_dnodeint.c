d81b51c384fbd6bf2449362290092bc2d33ab4a1
bamansour1978 <karatman@gmail.com>
2023-06-15 16:46:09 -0700

    FIx

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -26,15 +26,23 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 	}
 
-	ne_node->n = n;
+	new_node->n = n;
 	new_node->prev = NULL;
 	new_node->next = *head;
 
 	if (*head != NULL)
 	{
-		(*head)->prev = new_node;
+		new_node->prev = NULL;
+		new_node->next = *head;
+		new_node->n = n;
+		*head = new_node;
 	}
-
-	*head = new_node;
+	else
+	{
+		*head = new_node;
+		new_node->n = n;
+		new_node->prev = NULL;
+		new_node->next = NULL;
+	}
 	return (*head);
 }
