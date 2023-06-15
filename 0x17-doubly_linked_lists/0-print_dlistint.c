161c6bb9b18d7eeaee635ad76b5a92d3a6c06400
bamansour1978 <karatman@gmail.com>
2023-06-15 15:10:58 -0700

    FIx Error

--- 0x17-doubly_linked_lists/0-print_dlistint.c
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -11,13 +11,13 @@
  */
 size_t print_dlistint(const dlistint_t *h)
 {
-	int j;
+	size_t j = 0;
 
-	if (h == 0)
+	if (h == NULL)
 	{
 		return (0);
 	}
-	while (h)
+	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		j++;
