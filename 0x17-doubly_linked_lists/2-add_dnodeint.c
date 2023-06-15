9f14328e58a7adc46a349f604598276f81392c00
bamansour1978 <karatman@gmail.com>
2023-06-15 16:55:31 -0700

    ERror

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -15,11 +15,6 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 {
 	dlistint_t *new_node;
 
-	if (head == NULL)
-	{
-		return (NULL);
-	}
-
 	new_node = malloc(sizeof(dlistint_t));
 	if (new_node == NULL)
 	{
@@ -32,17 +27,9 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 
 	if (*head != NULL)
 	{
-		new_node->prev = NULL;
-		new_node->next = *head;
-		new_node->n = n;
-		*head = new_node;
+		(*head)->prev = new_node;
 	}
-	else
-	{
-		*head = new_node;
-		new_node->n = n;
-		new_node->prev = NULL;
-		new_node->next = NULL;
-	}
-	return (*head);
+
+	*head = new_node;
+	return (new_node);
 }
