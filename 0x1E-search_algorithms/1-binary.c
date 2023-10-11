b71df186a437f5f24a3797719120a7d5261d7411
bamansour1978 <karatman@gmail.com>
2023-10-11 21:23:50 +0100

    fix erreur edenting

--- 0x1E-search_algorithms/1-binary.c
+++ 0x1E-search_algorithms/1-binary.c
@@ -10,28 +10,28 @@
  */
 int binary_search(int *array, size_t size, int value)
 {
-size_t l, r, j;
+	size_t j, l, r;
 
-if (array == NULL)
-return (-1);
+	if (!array)
+		return (-1);
 
-l = 0;
-r = size - 1;
-while (r >= l)
-{
-printf("Searching in array: ");
+	l = 0;
+	r = size - 1;
+	while (r >= l)
+	{
 
-for (j = l ; j < r ; j++)
-printf("%d\n", array[j]);
-printf("%d\n", array[j]);
+		printf("Searching in array: ");
+		for (j = l; j < r; j++)
+			printf("%d, ", array[j]);
+		printf("%d\n", array[j]);
 
-j = (r - l) / 2 + l;
-if (array[j] < value)
-l = j + 1;
-else if (array[j] > value)
-r = j - 1;
-else
-return (j);
-}
-return (-1);
+		j = l + (r - l) / 2;
+		if (array[j] < value)
+			l = j + 1;
+		else if (array[j] > value)
+			r = j - 1;
+		else
+			return (j);
+	}
+	return (-1);
 }
